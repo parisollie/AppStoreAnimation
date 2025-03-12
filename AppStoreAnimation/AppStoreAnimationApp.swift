4c65f074bb65343d53eaca11934eee2bc21a88e1
parisollie <pjff18@gmail.com>
2025-03-12 10:36:17 -0600

    Initial Commit

---
+++ AppStoreAnimation/AppStoreAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreAnimationApp.swift
+//  AppStoreAnimation
+//
+//  Created by Paul F on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppStoreAnimation/View/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  AppStoreAnimation
+//
+//  Created by Paul F on 12/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+    }
+}
+
+#Preview {
+    ContentView()
+}
